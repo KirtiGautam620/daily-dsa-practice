0913b0233bbd42c8ad4500adc4bdfc08c7428ce7
Kirti Gautam <gautamkirti620@gmail.com>
2026-08-29 23:44:36 +0530

    day4: max consecutive sums

---
+++ 01-arrays/max-consecutive-ones.py
@@ -0,0 +1,21 @@
+'''
+Problem: Two Sum
+LeetCode: #485
+Difficulty: Easy
+Pattern: Array
+Status: Independent
+Date: 2026-08-29
+'''
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        c=0
+        mx=0
+        for i in range(len(nums)):
+            if nums[i]==0:
+                mx=max(mx,c)
+                c=0
+            else:
+                c+=1
+        mx=max(mx,c)
+        return mx
