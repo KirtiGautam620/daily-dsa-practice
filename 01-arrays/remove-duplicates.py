7981d90bfd8db382e2b7cd561dd22993d277bd1e
Kirti Gautam <gautamkirti620@gmail.com>
2026-08-29 23:06:55 +0530

    remove duplicates

--- 01-arrays/remove-duplicates.py
+++ 01-arrays/remove-duplicates.py
@@ -8,6 +8,11 @@ Date: 2026-08-27
 '''
 
 class Solution:
-    def removeDuplicates(self, nums):
-        n=set(nums)
-        return len(n)
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j=0
+        for i in range(1,len(nums)):
+            if nums[j]!=nums[i]:
+                j+=1
+                nums[j]=nums[i]
+        return j+1        
+        
